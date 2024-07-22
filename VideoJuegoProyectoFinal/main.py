75b20b110e6177fb1163bb37e014d71dcf607e1a
DianyLiz <dlenamorado.uth.hn>
2024-07-22 10:04:37 -0600

    Dibujando Camino del VideoJuego y Recorrido del Enemigo (22-07-2024)

--- VideoJuegoProyectoFinal/main.py
+++ VideoJuegoProyectoFinal/main.py
@@ -3,7 +3,9 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 
 import pygame as pg
+import json
 from enemy import Enemy
+from world import World
 import constants as c
 pg.init()
 
@@ -14,19 +16,26 @@ clock = pg.time.Clock()
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption("Tower Defence")
 
+map_image = pg.image.load("levels/level.png").convert_alpha()
+
 enemy_image = pg.image.load("imagen/enemy_1.png").convert_alpha()
+
+#Cargar el archivo json
+with open("levels/level.tmj") as file:
+    world_data = json.load(file)
+
+
+#crear un mundo
+
+world = World(world_data,map_image)
+
+world.process_data()
 
 #crear un grupo de imagenes
 enemy_group = pg.sprite.Group()
 
-waypoints = [
-    (100,100),
-    (400,200),
-    (400,100),
-    (200,300)
-]
 
-enemy = Enemy(waypoints,enemy_image)
+enemy = Enemy(world.waypoints,enemy_image)
 
 #Añadir el imagen al grupo
 enemy_group.add(enemy)
@@ -37,8 +46,10 @@ while run:
     clock.tick(c.FPS)
     screen.fill("grey100")
 
-    #dibujar el camino enemigo
-    pg.draw.lines(screen,"grey0",False,waypoints)
+    #dibujar el mundo
+    world.draw(screen)
+
+    
 
     #Actualiza los grupos
     enemy_group.update()
---
+++ VideoJuegoProyectoFinal/world.py
@@ -0,0 +1,30 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+import pygame as pg
+
+class World():
+    def __init__(self,data,map_image):
+        self.waypoints = []
+        self.level_data = data
+        self.image = map_image
+
+    def process_data(self):
+        #procesar los datos del mundo
+        for layer in self.level_data["layers"]:
+            if layer["name"] == "waypoints":
+                for obj in layer["objects"]:
+                    waypoint_data = obj["polyline"]
+                    self.process_waypoints(waypoint_data)
+
+    def process_waypoints(self,data):
+        #procesar los waypoints
+        for point in data:
+            temp_x = point.get("x")
+            temp_y = point.get("y")
+            print((temp_x,temp_y))
+            self.waypoints.append((temp_x,temp_y))
+
+
+    def draw(self,surface):
+        surface.blit(self.image,(0,0))
